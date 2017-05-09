5da09d9f4fb826f60247a1f6a5f7f3c09da8735e
grellyd <graham.brown137@gmail.com>
2017-05-08 17:53:08 -0700

    Fixed TwoFactor error

--- lib/merge_api/services/two_factor.ex
+++ lib/merge_api/services/two_factor.ex
@@ -1,8 +1,11 @@
 defmodule MergeApi.Services.TwoFactor do
   alias MergeApi.User
 
-  def check_token(token) do
-    if token == "12345" do
-      {ok:, Repo.get(User)}
+  def check_token(user, token) do
+    case token do
+      "12345" -> 
+        {:ok, Repo.get(user, :id)}
+      _ -> {:error, "Could not authenticate token"}
+    end
   end
 end
